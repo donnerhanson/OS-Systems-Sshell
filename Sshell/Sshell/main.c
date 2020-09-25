3d524c423170e97aeba3885133e911b95ef4a599
Donner Hanson <donnerhanson@yahoo.com>
2020-09-25 10:59:39 -0700

    initial commit

---
+++ Sshell/Sshell/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Sshell
+//
+//  Created by Donner Hanson on 9/25/20.
+//  Copyright © 2020 Donner Hanson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
